520777cc2c0852acc83506dd3406552062a5acab
Varvara Babkova <varvara.munday@gmail.com>
2019-09-14 09:34:52 -0500

    first canvas

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Canvas from "./Canvas"
+
+function App() {
+  return (
+    <div className="App">
+      <Canvas text = {"some text"}/>
+    </div>
+  );
+}
+
+export default App;
